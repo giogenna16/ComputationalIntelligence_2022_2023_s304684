71be968e8aa66ed4f2d205208c9dffd5811ff3b6
GIOVANNI GENNA s304684 <s304684@studenti.polito.it>
2023-01-21 16:27:31 +0100

    Added some functionalities to the RL agent and changed the main

--- quarto/main.py
+++ quarto/main.py
@@ -5,7 +5,8 @@ import logging
 import argparse
 import random
 import quarto
-from quarto.players import *
+from quarto.HardCodedplayers import *
+from quarto.RLagent import *
 import matplotlib.pyplot as plt
 
 
@@ -21,39 +22,48 @@ class RandomPlayer(quarto.Player):
     def place_piece(self) -> tuple[int, int]:
         return random.randint(0, 3), random.randint(0, 3)
 
+NUM_MATCHES= 60
 
-def main():
+def main_RL_agent():
     count_won= 0
     game = quarto.Quarto()
     robot= RenforcementLearningAgent(game)
     winsHistory= []
     indices= []
-    for m in range(50):
+    for m in range(NUM_MATCHES):
         game.reset()
         won= None
+        lost= None
         if m%2== 0:  # one time it starts first, another second
-            game.set_players((robot, HardCodedPlayer2(game)))
+            game.set_players((robot, HardCodedPlayer0(game)))
             winner= game.run()
             if winner== 0:
                 won= 1
+                lost= 0
                 count_won+= 1
             elif winner== 1:
                 won= 0
+                lost= 1
             else:
                 won= -1
+                lost= -1
         else:
-            game.set_players((HardCodedPlayer2(game), robot))
+            game.set_players((HardCodedPlayer0(game), robot))
             winner = game.run()
             if winner== 1:
                 won= 1
+                lost= 0
                 count_won+= 1
             elif winner== 0:
                 won= 0
+                lost= 1
             else:
                 won= -1
+                lost= -1
 
         #add the reward for the final state
-        reward= robot.get_reward(won)
+        reward_won= robot.get_reward(won)
+        reward_lost= robot.get_reward(lost)
         board_status= game.get_board_status()
         board_status_t= list()
         for r in range(game.BOARD_SIDE):
@@ -62,20 +72,50 @@ def main():
         is_in, state_in_G= state_or_equivalent_in_G(board_status_t, robot.G)
         if not is_in:
             robot.G[board_status_t]= 0
-            robot.update_state_history(board_status_t, reward)  
+            robot.update_state_history(board_status_t, reward_won) 
+            robot.update_state_history_opponent(board_status_t, reward_lost)
         else:
-            robot.update_state_history(state_in_G, reward)
+            robot.update_state_history(state_in_G, reward_won)
+            robot.update_state_history_opponent(state_in_G, reward_lost)
         robot.learn()
 
-        if m % 20 == 0: 
+        if m%6== 0: 
             winsHistory.append(count_won)
             indices.append(m)
             count_won= 0
-    plt.plot(indices, winsHistory, label= "won matches")
+    #print(robot.G)
+    print(winsHistory)
+    print(sum(winsHistory))
+    plt.plot(indices, winsHistory)
     plt.xlabel('Number of matches')
-    plt.ylabel('Number of won matches for every 5 matches')
+    plt.ylabel('Number of won matches for every 6 matches')
     plt.title('How the Reinforcement Learning Agent works:\n')
     plt.show()
+
+
+def main_hard_coded_players():
+    game = quarto.Quarto()
+    won_matches=0
+    tied_matches=0
+    for m in range(NUM_MATCHES):
+        game.reset()
+        if m%2== 0:  # one time it starts first, another second
+            game.set_players((HardCodedPlayer2(game), RandomPlayer(game)))
+            winner= game.run()
+            if winner== 0:
+                won_matches+= 1
+            if winner== -1:
+                tied_matches+=1
+        else:
+            game.set_players((RandomPlayer(game), HardCodedPlayer2(game)))
+            winner = game.run()
+            if winner== 1:
+               won_matches+= 1
+            if winner== -1:
+                tied_matches+=1
+    print(f"The evaluated player won {won_matches} matches and tied {tied_matches} matches")
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -95,4 +135,5 @@ if __name__ == '__main__':
     elif args.verbose == 2:
         logging.getLogger().setLevel(level=logging.DEBUG)
 
-    main()
+    main_RL_agent()
+    #main_hard_coded_players()
---
+++ quarto/quarto/RLagent.py
@@ -0,0 +1,194 @@
+import random
+from quarto.objects import Player, Quarto
+from copy import deepcopy
+
+class RenforcementLearningAgent(Player):
+    def __init__(self, quarto: Quarto, alpha=0.15, random_factor=0.2) -> None:# 80% explore, 20% exploit
+        super().__init__(quarto)
+        self.state_history = []  # state, reward
+        self.state_history_opponent= []
+        self.alpha = alpha
+        self.random_factor = random_factor
+        self.G = {}
+
+    def choose_piece(self)->  int:
+        game= self.get_game()
+        board_status= game.get_board_status()
+
+        placed= []  # pieces already placed
+        not_placed= []  # pieces have not been placed yet; in this case, they corresponds to the "allowed moves"
+        minG = 10e15
+        chosen_piece= None
+        for j in range(game.BOARD_SIDE):
+            for i in range(game.BOARD_SIDE):
+                p= board_status[j, i]
+                if p!= -1: # occupied position
+                    placed.append(p)
+        for i in range(16):
+            if i not in placed:
+                not_placed.append(i)
+
+        # if random.random() < self.random_factor:
+        #     # if random number below random factor, choose random action
+        #     chosen_piece= random.choice(not_placed)
+        # else: #if leave the comment, tab the following for
+        for p in not_placed:
+            average_reward= 0 # for each p, in not_placed, so among the allowed pieces, compute the "average_reward", i.e. the sum of all the rewards of the states created placing p in all the different free positions
+            for j in range(game.BOARD_SIDE):
+                for i in range(game.BOARD_SIDE):
+                    pos= board_status[j, i] # the value of the (j,i) position on the board
+                    if pos== -1: # free position
+                        tmp= deepcopy(game)
+                        tmp.select(p) # select an element from the not placed yet list
+                        tmp.place(i, j) # place the selected element in the (j, i) position
+                        tmp_board_t= list()
+                        tmp_board= tmp.get_board_status()
+                        for r in range(tmp.BOARD_SIDE):
+                            tmp_board_t+= tmp_board[r].tolist()
+                        tmp_board_t= tuple(tmp_board_t)
+                        is_in, state_in_G= state_or_equivalent_in_G(tmp_board_t, self.G)
+                        if is_in:     
+                            average_reward+= self.G[state_in_G]
+            # select the piece with the minimum average point, because it will be the piece for the opponent
+            if average_reward<= minG:
+                minG= average_reward
+                chosen_piece= p
+
+        if chosen_piece== None:
+            chosen_piece= random.choice(not_placed)
+
+        return chosen_piece
+    
+    def place_piece(self)-> tuple[int, int]:
+        game= self.get_game()
+        board_status= game.get_board_status()  
+        
+        # inizialize the reward of the state before you place piece, so after opponent's ply and save it in
+        # the state history of the opponent
+        board_status_t= list()
+        for r in range(game.BOARD_SIDE):
+            board_status_t+= board_status[r].tolist()
+        board_status_t= tuple(board_status_t)
+        is_in, state_in_G= state_or_equivalent_in_G(board_status_t, self.G)
+        reward= self.get_reward() 
+        if not is_in:
+            self.G[board_status_t]= 0 
+            self.update_state_history_opponent(board_status_t, reward)
+        else:
+            self.update_state_history_opponent(state_in_G, reward)
+           
+        allowed_positions= []
+        maxG = -10e15
+        chosen_position= None
+        for j in range(game.BOARD_SIDE):
+            for i in range(game.BOARD_SIDE):
+                p= board_status[j, i] # the value of the (j,i) position on the board
+                if p== -1: # free position
+                    allowed_positions.append((j, i))
+                    
+        if random.random() < self.random_factor:
+            # if random number below random factor, choose random action
+            chosen_position= random.choice(allowed_positions)
+            chosen_position= (chosen_position[1], chosen_position[0])
+        else:
+            for board_pos in allowed_positions:
+                tmp= deepcopy(game)
+                tmp.place(board_pos[1], board_pos[0]) # place the selected element in the (j, i) position
+                tmp_board_t= list()
+                tmp_board= tmp.get_board_status()
+                for r in range(tmp.BOARD_SIDE):
+                    tmp_board_t+= tmp_board[r].tolist()
+                tmp_board_t= tuple(tmp_board_t)
+                is_in, state_in_G= state_or_equivalent_in_G(tmp_board_t, self.G)
+                if not is_in:
+                    if maxG< 0: # if the state with the chosen position is not in G and maxG is negative, set maxG to 0
+                        maxG= 0
+                        chosen_position= (board_pos[1], board_pos[0])
+                else:
+                    if self.G[state_in_G]>= maxG:
+                        maxG= self.G[state_in_G]
+                        chosen_position= (board_pos[1], board_pos[0])
+        
+        # inizialize the reward of the status with the chosen position and save it in your state history 
+        tmp= deepcopy(game)
+        tmp.place(chosen_position[0], chosen_position[1]) # place the selected element in the (j, i) position
+        tmp_board_t= list()
+        tmp_board= tmp.get_board_status()
+        for r in range(tmp.BOARD_SIDE):
+            tmp_board_t+= tmp_board[r].tolist()
+        tmp_board_t= tuple(tmp_board_t)
+        is_in, state_in_G= state_or_equivalent_in_G(tmp_board_t, self.G)
+        reward= self.get_reward() 
+        if not is_in:
+            self.G[tmp_board_t]= 0 
+            self.update_state_history(tmp_board_t, reward)
+        else:
+            self.update_state_history(state_in_G, reward)
+        
+        return chosen_position
+
+    def get_reward(self, won= None):
+        if won!= None: 
+            if won== 1:
+                return 3 # if the agent won
+            elif won== 0:
+                return -3 # if the agent lost  
+            else:
+                return 1  # on par    
+        else:
+            return 0 # if it is not the end of the match
+
+    def update_state_history(self, state, reward):
+        self.state_history.append((state, reward))
+    
+    def update_state_history_opponent(self, state, reward):
+        self.state_history_opponent.append((state, reward))
+
+    def learn(self):
+        target = 0
+        for prev, reward in reversed(self.state_history):
+            self.G[prev] = self.G[prev] + self.alpha * (target - self.G[prev])
+            target += reward
+        target = 0
+        for prev, reward in reversed(self.state_history_opponent):
+            self.G[prev] = self.G[prev] + self.alpha * (target - self.G[prev])
+            target += reward
+        self.state_history = []
+        self.state_history_opponent = []
+        self.random_factor -= 10e-5  # decrease random factor each episode of play
+
+
+#-----------------------------------------------------------------------------------------------#
+
+def ninghty_degree_rotation(state: tuple):
+    # Rotation of 90 degrees
+    rotated_state= [0]*len(state) # a list of 16 zeros
+    dict_corr= {0: 12, 1: 8, 2: 4, 3: 0} # index matches for rotation
+    for i in range(len(state)):
+        for j in range(len(state)//4):
+            if i%4== j:
+                for k in range(len(state)//4):
+                    if i//4== k:
+                        rotated_state[dict_corr[j]+k]= state[i]  
+    return tuple(rotated_state)
+
+def state_or_equivalent_in_G(state: tuple, G: dict):
+    # checks if the state or one of its rotations of 90, 180, 270 degrees 
+    # (which are equivalent to it) is present in G
+    ninghty_rotated_state= ninghty_degree_rotation(state)
+    onehundredeighty_rotated_state= ninghty_degree_rotation(ninghty_rotated_state)
+    twohundredseventy_rotated_state= ninghty_degree_rotation(onehundredeighty_rotated_state)
+    #return True if a state or one of its equivalents is in G, return also the state which is in G
+    if state in G:
+        return True, state
+    elif ninghty_rotated_state in G:
+        return True, ninghty_rotated_state
+    elif onehundredeighty_rotated_state in G:
+        return True, onehundredeighty_rotated_state
+    elif twohundredseventy_rotated_state in G:
+        return True, twohundredseventy_rotated_state
+    else:
+        return False, ()
+
+
+    
